ed875e38a5514b65454d98cb4b11ec3a0855c146
Christian <christianfontes82@gmail.com>
2015-12-29 15:47:33 -0430

    modificaciones

--- templates/base/index.php
+++ templates/base/index.php
@@ -312,11 +312,7 @@ padding: 30px 0;
     <script type="text/javascript" src="libs/advertisement.js"></script>
     <script type="text/javascript" src="libs/check.js"></script>
     <?php endif; ?>
-    </div>
-    </div>
-    <div class="container">
-    <div class="row">
-    <div id="page">
+    <br></br>
     <section>
         <p>What is Bitcoin?</p>
         <p>Bitcoin is a payment system introduced as open-source software in 2009 by developer Satoshi Nakamoto.</p>
@@ -328,7 +324,6 @@ padding: 30px 0;
         <p>Besides mining, bitcoins can be obtained in exchange for fiat money, products, and services.</p>
         <p>Users can send and receive bitcoins electronically for an optional transaction fee using</p> <p> wallet software on a personal computer, mobile device, or a web application.</p>
     </section>
-    </div>
     </div>
     </div>
 </div>
