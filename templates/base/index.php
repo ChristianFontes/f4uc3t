b2b06533d6b3037d08846c92aa8de6d932e6dd2c
Christian <christianfontes82@gmail.com>
2015-12-29 20:51:18 -0430

    modificaciones

--- templates/base/index.php
+++ templates/base/index.php
@@ -359,16 +359,6 @@ padding: 30px 0;
         -->
     </tr>
     </table>
-    <script type="text/javascript">var _Hasync= _Hasync|| [];
-    _Hasync.push(['Histats.start', '1,3001481,4,2047,280,25,00010001']);
-    _Hasync.push(['Histats.fasi', '1']);
-    _Hasync.push(['Histats.track_hits', '']);
-    (function() {
-    var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-    hs.src = ('http://s10.histats.com/js15_as.js');
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-    })();</script>
-    <noscript><a href="http://www.histats.com" target="_blank"><img  src="http://sstatic1.histats.com/0.gif?3001481&101" alt="php hit counter" border="0"></a></noscript>
     </div>
     </div>
 </div>
