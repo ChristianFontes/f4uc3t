905d3e5209f4eb28982a32e0ae46611c172d95bc
Christian Fontes <Christian Fontes>
2016-01-03 14:49:32 -0430

    new

--- config.php
+++ config.php
@@ -1,13 +1,33 @@
 <?php
 // Your password: mnQ25n4eZoFKRod. King
 // Your password: pqorap44oSRSDPY. Like
-
+// King
+/* 
 $dbhost = "mysql.hostinger.es";
 $dbuser = "u262227228_user";
 $dbpass = "ajZ0uSm4Q4V";
 $dbname = "u262227228_bd";
 $display_errors = false;
 $disable_admin_panel = false;
+*/
+// Satoshistoday
+
+$dbhost = "mysql.hostinger.es";
+$dbuser = "u756791466_user";
+$dbpass = "ajZ0uSm4Q4V";
+$dbname = "u756791466_db";
+$display_errors = false;
+$disable_admin_panel = false;
+
+// Like
+/*
+$dbhost = "mysql.hostinger.es";
+$dbuser = "u262227228_user";
+$dbpass = "ajZ0uSm4Q4V";
+$dbname = "u262227228_bd";
+$display_errors = false;
+$disable_admin_panel = false;
+*/
 
 $connection_options = array(
     'disable_curl' => false,
