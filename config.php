37dbdb9d77720f7da3aa49f2d8252beb0e68d9f5
Christian Fontes <Christian Fontes>
2016-01-08 18:58:18 -0430

    fondo

--- config.php
+++ config.php
@@ -2,24 +2,24 @@
 // Your password: mnQ25n4eZoFKRod. King
 // Your password: pqorap44oSRSDPY. Like
 // King
-
+/*
 $dbhost = "mysql.hostinger.es";
 $dbuser = "u262227228_user";
 $dbpass = "ajZ0uSm4Q4V";
 $dbname = "u262227228_bd";
 $display_errors = false;
 $disable_admin_panel = false;
-
+*/
 // Satoshistoday
 // nPiPMjC8d5ZfzTi
-/*
+
 $dbhost = "mysql.hostinger.es";
 $dbuser = "u756791466_user";
 $dbpass = "ajZ0uSm4Q4V";
 $dbname = "u756791466_db";
 $display_errors = false;
 $disable_admin_panel = false;
-*/
+
 // Like
 /*
 $dbhost = "mysql.hostinger.es";
